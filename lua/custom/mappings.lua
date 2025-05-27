b30e5111332fc355929d4c032ead7abc8f66bbda
mualifin22 <mimualifin22@gmail.com>
2025-05-28 06:41:22 +0700

    update

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -9,3 +9,12 @@ vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
     and "<Plug>luasnip-jump-prev"
     or "<S-Tab>"
 end, { expr = true })
+
+
+local map = vim.keymap.set
+
+-- Jalankan Laravel serve dari dalam Neovim
+map("n", "<leader>as", ":term php artisan serve<CR>", { desc = "Laravel Serve" })
+
+-- Jalankan PHPUnit
+map("n", "<leader>at", ":term ./vendor/bin/phpunit<CR>", { desc = "Run PHPUnit" })
